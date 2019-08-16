57713574a3ea285e1725bc3911fec3ed96327400
Mizuki Zenta <mizukisb@gmail.com>
2019-08-16 15:06:11 +1000

    added admin and encripted password

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_051330) do
+ActiveRecord::Schema.define(version: 2019_08_16_045409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "appointments", force: :cascade do |t|
     t.bigint "client_id"
